bff887b6e2d56855e889519ced51b200f305f0d2
ufoo68 <mitarou.yuutarou@gmail.com>
2020-05-03 00:11:20 +0900

    Prepared

---
+++ src/graphql/queries.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const allItem = /* GraphQL */ `
+  query AllItem {
+    allItem {
+      id
+      data
+    }
+  }
+`;
