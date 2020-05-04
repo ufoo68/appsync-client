6dd7a92db480c391bb82d75e9f5f35b4fb1b2c7e
ufoo68 <mitarou.yuutarou@gmail.com>
2020-05-05 08:37:54 +0900

    implement mutation

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { useState } from 'react'
 import { useEffectAsync } from '@availity/hooks'
 import { API, graphqlOperation } from 'aws-amplify'
 import { allItem } from './graphql/queries'
+import { addItem } from './graphql/mutations'
 
 const App = () => {
   const [item, setItem] = useState([{
     id: '',
     data: '',
   }])
+  const [inputItem, setInputItem] = useState({
+    data: '',
+  })
   useEffectAsync(async () => {
     const result = await API.graphql(graphqlOperation(allItem))
     setItem(result.data.allItem)
   })
+
   return (
-    <ul>
-      {item.map(i =>
-        <li>
-          {i.data}
-        </li>
-      )}
-    </ul>
+    <div>
+      <input value={inputItem.data} onChange={(e) => setInputItem({ data: e.target.value})} />
+      <button onClick={
+        async () => await API.graphql(graphqlOperation(addItem, { item: inputItem }))
+      }>
+        add
+      </button>
+      <ul>
+        {item.map(i =>
+          <li>
+            {i.data}
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
 
